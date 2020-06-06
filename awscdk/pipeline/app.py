269ed033dd7f0ca6259f3f9256f8cf7a2609c86a
Michael Hoffmann <michaelh@centaur.id.au>
2020-06-06 17:05:52 +1000

    add branch specifier, as we won't be mucking with master for this exercise codepipeline trigger will be develop

--- awscdk/pipeline/app.py
+++ awscdk/pipeline/app.py
@@ -15,6 +15,7 @@ smname = pipeline_config['githubtokenname']
 smkey = pipeline_config['githubtokenkey']
 owner = pipeline_config['repoowner']
 repo = pipeline_config['reponame']
+branch = pipeline_config['buildbranch']
 
 role_arn = f'arn:aws:iam::{account_id}:role/CentaurDev'
 smarn = f'arn:aws:secretsmanager:{region}:{account_id}:secret:{smname}'
@@ -22,6 +23,6 @@ smarn = f'arn:aws:secretsmanager:{region}:{account_id}:secret:{smname}'
 app = core.App()
 
 TTAPipeline(app, 'TTAPipeline', env=core.Environment(region=region, account=account_id), rolearn=role_arn,
-            githubtokenarn=smarn, githubtokenkey=smkey, githubrepoowner=owner, githubrepo=repo)
+            githubtokenarn=smarn, githubtokenkey=smkey, githubrepoowner=owner, githubrepo=repo, buildbranch=branch)
 
 app.synth()
--- awscdk/pipeline/techtestapp_pipeline/techtestapppipeline.py
+++ awscdk/pipeline/techtestapp_pipeline/techtestapppipeline.py
@@ -36,13 +36,13 @@ from aws_cdk import \
 class TTAPipeline(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, *,
-                 rolearn, githubtokenarn, githubtokenkey, githubrepoowner, githubrepo,
+                 rolearn, githubtokenarn, githubtokenkey, githubrepoowner, githubrepo, buildbranch='master',
                  **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         pipeline = cpl.Pipeline(self, "TTAPipeline", pipeline_name='TTAPipeline')
 
-        source_action, source_artifact = self.get_source(owner=githubrepoowner, repo=githubrepo,
+        source_action, source_artifact = self.get_source(owner=githubrepoowner, repo=githubrepo, branch=buildbranch,
                                                          smarn=githubtokenarn, smkey=githubtokenkey)
         pipeline.add_stage(stage_name="TTASource", actions=[source_action])
 
@@ -52,13 +52,14 @@ class TTAPipeline(core.Stack):
         # deploy_action = self.get_deploy(buildartifact=build_artifact)
         # pipeline.add_stage(stage_name="TTADeploy", actions=[deploy_action])
 
-    def get_source(self, *, owner, repo, smarn, smkey):
+    def get_source(self, *, owner, repo, branch='master', smarn, smkey):
         source_artifact = cpl.Artifact()
         source_action = cpactions.GitHubSourceAction(
             action_name="Github_Source",
             output=source_artifact,
-            owner="Marakai",
-            repo="TechTestApp",
+            owner=owner,
+            repo=repo,
+            branch=branch,
             oauth_token=self.get_token(smarn=smarn, smkey=smkey)
         )
         return source_action, source_artifact
