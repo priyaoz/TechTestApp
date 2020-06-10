f1dfc61e5d4bde9bc3e22f9ba51bcf618e15f941
Michael Hoffmann <michaelh@centaur.id.au>
2020-06-11 00:07:18 +1000

    OK, didn't work from within our VPC, I suspect it's the endpoint that has to be temporarily routable to the Internet.

--- awscdk/transferpw/dbinit.sh
+++ awscdk/transferpw/dbinit.sh
@@ -1,12 +1,16 @@
-source ./vpcdata.sh
 
-pcx=$(aws ec2 create-vpc-peering-connection --vpc-id ${vpc1} --peer-vpc-id ${vpc2} --query 'VpcPeeringConnection.VpcPeeringConnectionId' --output text)
-aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id ${pcx}
+# temporarily attach an IGW and routes to the DB subnets, just to init that damn DB
 
-aws ec2 create-route --route-table-id ${rtb1} --destination-cidr-block ${cidr2} --vpc-peering-connection-id ${pcx}
-aws ec2 create-route --route-table-id ${rtb21} --destination-cidr-block ${cidr1} --vpc-peering-connection-id ${pcx}
-aws ec2 create-route --route-table-id ${rtb22} --destination-cidr-block ${cidr1} --vpc-peering-connection-id ${pcx}
-aws ec2 create-route --route-table-id ${rtb23} --destination-cidr-block ${cidr1} --vpc-peering-connection-id ${pcx}
+vpc=$(aws ec2 describe-vpcs --filter 'Name=cidr,Values=10.0.0.0/16' --query 'Vpcs[0].VpcId' --output text)
+igw=$(aws ec2 describe-internet-gateways --filters 'Name=tag:aws:cloudformation:stack-name,Values=TTAVPC' --query 'InternetGateways[].InternetGatewayId' --output text)
+
+rtb1=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet1' --query 'RouteTables[].RouteTableId' --output text)
+rtb2=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet2' --query 'RouteTables[].RouteTableId' --output text)
+rtb3=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet3' --query 'RouteTables[].RouteTableId' --output text)
+
+aws ec2 create-route --route-table-id "${rtb1}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
+aws ec2 create-route --route-table-id "${rtb2}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
+aws ec2 create-route --route-table-id "${rtb3}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
 
 dbep=$(aws rds describe-db-cluster-endpoints --query 'DBClusterEndpoints[?EndpointType==`WRITER`].Endpoint' --output text)
 docker run \
@@ -19,11 +23,8 @@ docker run \
 	-e VTT_LISTENPORT=3000 \
 	102460195799.dkr.ecr.ap-southeast-2.amazonaws.com/techtestapp_ecr:latest updatedb -s
 
-# aws ec2 delete-route --route-table-id ${rtb1} --destination-cidr-block ${cidr2}
-# aws ec2 delete-route --route-table-id ${rtb21} --destination-cidr-block ${cidr1}
-# aws ec2 delete-route --route-table-id ${rtb22} --destination-cidr-block ${cidr1}
-# aws ec2 delete-route --route-table-id ${rtb23} --destination-cidr-block ${cidr1}
-
-# aws ec2 delete-vpc-peering-connection --vpc-peering-connection-id ${pcx}
+# aws ec2 delete-route --route-table-id "${rtb21}" --destination-cidr-block '0.0.0.0/0'
+# aws ec2 delete-route --route-table-id "${rtb22}" --destination-cidr-block '0.0.0.0/0'
+# aws ec2 delete-route --route-table-id "${rtb23}" --destination-cidr-block '0.0.0.0/0'
 
 exit 0
--- awscdk/transferpw/vpcdata.sh
+++
@@ -1,9 +0,0 @@
-# source me!
-vpc1='vpc-08d90242a8b69c703'
-vpc2='vpc-057facb00bc339b20'
-rtb1='rtb-005c468476ee4b045'
-rtb21='rtb-08fe6b5305cb11f06'
-rtb22='rtb-07240bea7d0b2c08f'
-rtb23='rtb-0ee8f02515c2dd38c'
-cidr1='172.16.128.0/18'
-cidr2='10.0.0.0/16'
