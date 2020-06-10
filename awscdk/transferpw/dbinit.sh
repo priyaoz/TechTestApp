3cba98746f97a70c76910aff39ec791103ac89d3
Michael Hoffmann <michaelh@centaur.id.au>
2020-06-11 00:32:13 +1000

    that did not work. now trying to rework with private (instead of isolated subnets) and NAT gateway. this is starting to cost a fortune!

--- awscdk/transferpw/dbinit.sh
+++ awscdk/transferpw/dbinit.sh
@@ -1,16 +1,16 @@
 
 # temporarily attach an IGW and routes to the DB subnets, just to init that damn DB
 
-vpc=$(aws ec2 describe-vpcs --filter 'Name=cidr,Values=10.0.0.0/16' --query 'Vpcs[0].VpcId' --output text)
-igw=$(aws ec2 describe-internet-gateways --filters 'Name=tag:aws:cloudformation:stack-name,Values=TTAVPC' --query 'InternetGateways[].InternetGatewayId' --output text)
+# vpc=$(aws ec2 describe-vpcs --filter 'Name=cidr,Values=10.0.0.0/16' --query 'Vpcs[0].VpcId' --output text)
+# igw=$(aws ec2 describe-internet-gateways --filters 'Name=tag:aws:cloudformation:stack-name,Values=TTAVPC' --query 'InternetGateways[].InternetGatewayId' --output text)
 
-rtb1=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet1' --query 'RouteTables[].RouteTableId' --output text)
-rtb2=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet2' --query 'RouteTables[].RouteTableId' --output text)
-rtb3=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet3' --query 'RouteTables[].RouteTableId' --output text)
+# rtb1=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet1' --query 'RouteTables[].RouteTableId' --output text)
+# rtb2=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet2' --query 'RouteTables[].RouteTableId' --output text)
+# rtb3=$(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=vpc-057facb00bc339b20" 'Name=tag:Name,Values=TTAVPC/VPC/DBSubnet3' --query 'RouteTables[].RouteTableId' --output text)
 
-aws ec2 create-route --route-table-id "${rtb1}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
-aws ec2 create-route --route-table-id "${rtb2}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
-aws ec2 create-route --route-table-id "${rtb3}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
+# aws ec2 create-route --route-table-id "${rtb1}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
+# aws ec2 create-route --route-table-id "${rtb2}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
+# aws ec2 create-route --route-table-id "${rtb3}" --destination-cidr-block '0.0.0.0/0' --gateway-id "${igw}"
 
 dbep=$(aws rds describe-db-cluster-endpoints --query 'DBClusterEndpoints[?EndpointType==`WRITER`].Endpoint' --output text)
 docker run \
