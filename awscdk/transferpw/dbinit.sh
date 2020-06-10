a6df5acc8e5540d03b505a3c13c694965ed7df88
Michael Hoffmann <michaelh@centaur.id.au>
2020-06-10 23:37:56 +1000

    now with configurable simple script that bring up temporary VPC peering from what we hope is the network running codebuild into the DB subnets

--- awscdk/transferpw/dbinit.sh
+++ awscdk/transferpw/dbinit.sh
@@ -1,3 +1,12 @@
+source ./vpcdata.sh
+
+pcx=$(aws ec2 create-vpc-peering-connection --vpc-id ${vpc1} --peer-vpc-id ${vpc2} --query 'VpcPeeringConnection.VpcPeeringConnectionId' --output text)
+aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id ${pcx}
+
+aws ec2 create-route --route-table-id ${rtb1} --destination-cidr-block ${cidr2} --vpc-peering-connection-id ${pcx}
+aws ec2 create-route --route-table-id ${rtb21} --destination-cidr-block ${cidr1} --vpc-peering-connection-id ${pcx}
+aws ec2 create-route --route-table-id ${rtb22} --destination-cidr-block ${cidr1} --vpc-peering-connection-id ${pcx}
+aws ec2 create-route --route-table-id ${rtb23} --destination-cidr-block ${cidr1} --vpc-peering-connection-id ${pcx}
 
 dbep=$(aws rds describe-db-cluster-endpoints --query 'DBClusterEndpoints[?EndpointType==`WRITER`].Endpoint' --output text)
 docker run \
@@ -10,3 +19,11 @@ docker run \
 	-e VTT_LISTENPORT=3000 \
 	102460195799.dkr.ecr.ap-southeast-2.amazonaws.com/techtestapp_ecr:latest updatedb -s
 
+# aws ec2 delete-route --route-table-id ${rtb1} --destination-cidr-block ${cidr2}
+# aws ec2 delete-route --route-table-id ${rtb21} --destination-cidr-block ${cidr1}
+# aws ec2 delete-route --route-table-id ${rtb22} --destination-cidr-block ${cidr1}
+# aws ec2 delete-route --route-table-id ${rtb23} --destination-cidr-block ${cidr1}
+
+# aws ec2 delete-vpc-peering-connection --vpc-peering-connection-id ${pcx}
+
+exit 0
